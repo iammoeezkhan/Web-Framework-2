a05df402bc75ca7e2489e1716976b4823ff476a8
moeez <moeezk115@gmail.com>
2022-09-13 13:29:07 +0300

    first   commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ const [Age, setAge] = useState(0)
+ const [Upper, setUpper] = useState(120)
+ const [Lower, setLower] = useState(90)
+
+ 
+ function calculate() {
+  const result1 = (220-Age)*0.85
+  setUpper(result1)
+  const result2 = (220-Age)*0.65
+  setLower(result2)
+  
+
+ }
+    return(
+      <div id="content">
+        <h3> Heart Rate Calculator</h3>
+        <form>
+          <div>
+            <label>Age:</label>
+            <input type="number" value ={Age} onChange={e=> setAge(e.target.value)}/>
+
+
+      </div>
+      <div>
+        
+          <label>Heart Rate:</label>
+      <output>{Lower.toFixed(0)}-{Upper.toFixed(0)}</output>
+
+      </div>
+      <button id="calculate" type="button" onClick={calculate}>Calculate</button>
+
+      </form>
+      </div>
+    );
+    }
+      
+
 
 export default App;
